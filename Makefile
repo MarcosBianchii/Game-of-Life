06d65f7f5dcb71b2562f79328cbac7f946a6dcd8
MarcosBianchii <marcosbianchi07@gmail.com>
2024-01-31 00:43:23 -0300

    last commit

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 FLAGS=-Wall -Wextra -Werror -std=c99 -pedantic -g -lncursesw
 
 all:
-	$(CC) main.c $(FLAGS) -o main
+	$(CC) main.c $(FLAGS) -o main && ./main
 
 clean:
 	rm -f main
--- main.c
+++ main.c
@@ -2,31 +2,44 @@
 #include <locale.h>
 #include <ncursesw/ncurses.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
 #define INIT_ALIVE_CHANCE 12
+#define TIMEOUT 70
 
-uint32_t N;
-uint32_t M;
+size_t SIZE = 1000;
+unsigned N;
+unsigned M;
 
-int *table[2][2] = {
-    {L" "     , L"\u2584"},
-    {L"\u2580", L"\u2588"},
+
+const int *table[2][2] = {
+    {L" ", L"▄"},
+    {L"▀", L"█"},
 };
 
-uint8_t n_neighbours(bool m[N][M], int64_t x, int64_t y) {
-    register int64_t row;
-    register int64_t col;
-    register uint8_t sum = 0;
 
-    for (int64_t i = 0; i < 3; i++) {
-        row = (x + i - 1) % N;
-        for (int64_t j = 0; j < 3; j++) {
-            col = (y + j - 1) % M;
+static inline unsigned
+positive_mod(int num, unsigned m)
+{
+    int mod = num % (int) m;
+    return m * (mod < 0) + mod;
+}
+
+
+unsigned
+n_neighbours(bool m[N][M], unsigned x, unsigned y)
+{
+    register unsigned row;
+    register unsigned col;
+    register unsigned sum = 0;
+
+    for (int i = -1; i < 2; i++) {
+        row = positive_mod(x + i, N);
+        for (int j = -1; j < 2; j++) {
+            col = positive_mod(y + j, M);
 
             if (row == x && col == y) {
                 continue;
@@ -41,16 +54,22 @@ uint8_t n_neighbours(bool m[N][M], int64_t x, int64_t y) {
     return sum;
 }
 
-bool check_cell(bool m[N][M], int64_t i, int64_t j) {
-    uint8_t n = n_neighbours(m, i, j);
+
+bool
+check_cell(bool m[N][M], unsigned i, unsigned j)
+{
+    unsigned n = n_neighbours(m, i, j);
     return (m[i][j] ? n == 2 : false) || n == 3;
 }
 
-void print_matrix(bool n[N][M]) {
+
+void
+print_matrix(bool n[N][M])
+{
     wchar_t buf[M]; buf[M] = 0;
 
-    for (uint32_t i = 0; i < N; i += 2) {
-        for (uint32_t j = 0; j < M; j++) {
+    for (unsigned i = 0; i < N; i += 2) {
+        for (unsigned j = 0; j < M; j++) {
             buf[j] = *table[n[i][j]][n[i + 1][j]];
         }
 
@@ -60,30 +79,28 @@ void print_matrix(bool n[N][M]) {
     refresh();
 }
 
-void game(bool n[N][M], bool m[N][M]) {
-    for (uint32_t i = 0; i < N; i++) {
-        for (uint32_t j = 0; j < M; j++) {
+
+void
+game(bool n[N][M], bool m[N][M])
+{
+    for (unsigned i = 0; i < N; i++) {
+        for (unsigned j = 0; j < M; j++) {
             n[i][j] = check_cell(m, i, j);
         }
     }
 
-    memcpy(m, n, sizeof(bool) * N * M);
+    memcpy(m, n, N * M * sizeof(bool));
     print_matrix(n);
+
+    // Update for resizes.
+    getmaxyx(stdscr, N, M);
+    N *= 2;
 }
 
-void init(bool n[N][M], bool m[N][M]) {
-    attron(COLOR_PAIR(rand() % 7 + 1));
 
-    for (uint32_t i = 0; i < N; i++) {
-        for (uint32_t j = 0; j < M; j++) {
-            n[i][j] = (rand() % 100) < INIT_ALIVE_CHANCE;
-        }
-    }
-
-    memcpy(m, n, sizeof(bool) * N * M);
-}
-
-void colors_init() {
+void
+colors_init()
+{
     start_color();
     attron(A_BOLD);
     use_default_colors();
@@ -96,29 +113,51 @@ void colors_init() {
     init_pair(7, COLOR_WHITE,   -1);
 }
 
-void curses_init() {
+
+void
+curses_init()
+{
     initscr();
     cbreak();
     noecho();
     curs_set(0);
-    timeout(70);
+    timeout(TIMEOUT);
     colors_init();
     getmaxyx(stdscr, N, M);
     N *= 2;
 }
 
-int main() {
+
+void
+init(bool n[N][M], bool m[N][M])
+{
+    attron(COLOR_PAIR(rand() % 7 + 1));
+
+    for (unsigned i = 0; i < N; i++) {
+        for (unsigned j = 0; j < M; j++) {
+            n[i][j] = (rand() % 100) < INIT_ALIVE_CHANCE;
+        }
+    }
+
+    memcpy(m, n, N * M * sizeof(bool));
+}
+
+
+int
+main()
+{
     srand(time(NULL));
     setlocale(LC_ALL, "");
     curses_init();
 
-    bool n[N][M];
-    bool m[N][M];
+    bool n[SIZE][SIZE];
+    bool m[SIZE][SIZE];
     init(n, m);
 
     int c = 0;
-    do (c = tolower(getch())) == 'r' ? init(n, m) : game(n, m);
-    while (c != 'q');
+    while ((c = tolower(getch())) != 'q') {
+        c == 'r' ? init(n, m) : game(n, m);
+    }
 
     endwin();
     return 0;
