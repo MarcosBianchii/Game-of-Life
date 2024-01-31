fb6d6529c12ed5c813647d8651b4e94f262c4f4c
MarcosBianchii <marcosbianchi07@gmail.com>
2024-01-30 22:50:16 -0300

    unicode dividing characters

--- game_of_life.c
+++ game_of_life.c
@@ -2,19 +2,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <ncurses.h>
+#include <ncursesw/ncurses.h>
 #include <ctype.h>
 #include <string.h>
 #include <time.h>
+#include <locale.h>
 
-uint64_t N;
-uint64_t M;
+#define INIT_ALIVE_CHANCE 12
+
+uint32_t N;
+uint32_t M;
+
+int *table[2][2] = {
+    {L" "     , L"\u2584"},
+    {L"\u2580", L"\u2588"},
+};
 
 uint8_t n_neighbours(bool m[N][M], int64_t x, int64_t y) {
-    uint8_t sum = 0;
-    for (int64_t i = x-1; i < x+2; i++) {
-        register int64_t row = i % N;
-        for (int64_t j = y-1; j < y+2; j++) {
+    register uint8_t sum = 0;
+    int64_t row;
+
+    for (int64_t i = x-1; i < x + 2; i++) {
+        row = i % N;
+        for (int64_t j = y-1; j < y + 2; j++) {
             int64_t col = j % M;
 
             sum += row == x && col == y ? 0 : m[row][col];
@@ -31,30 +41,42 @@ bool check_cell(bool m[N][M], int64_t i, int64_t j) {
 }
 
 void print_matrix(bool n[N][M]) {
-    char buf[M+1]; buf[M] = '\0';
-    for (uint64_t i = 0; i < N; i++) {
-        for (uint64_t j = 0; j < M; j++)
-            buf[j] = n[i][j] ? '#' : ' ';
-        mvprintw(i, 0, buf);
+    wchar_t buf[M]; buf[M] = 0;
+
+    for (uint32_t i = 0; i < N; i += 2) {
+        for (uint32_t j = 0; j < M; j++) {
+            buf[j] = *table[n[i][j]][n[i + 1][j]];
+        }
+
+        mvwprintw(stdscr, i / 2, 0, "%ls", buf);
     }
 
     refresh();
 }
 
 void game(bool n[N][M], bool m[N][M]) {
-    for (int64_t i = 0; i < N; i++)
-        for (int64_t j = 0; j < M; j++)
+    for (uint32_t i = 0; i < N; i++) {
+        for (uint32_t j = 0; j < M; j++) {
             n[i][j] = check_cell(m, i, j);
-    memcpy(m, n, sizeof(bool)*N*M);
+        }
+    }
+
+    memcpy(m, n, sizeof(bool) * N * M);
     print_matrix(n);
 }
 
 void init(bool n[N][M], bool m[N][M]) {
+    // Set random color to cells.
     attron(COLOR_PAIR(rand() % 7 + 1));
-    for (int64_t i = 0; i < N; i++)
-        for (int64_t j = 0; j < M; j++)
-            n[i][j] = M < N*N/(rand() % 10*M + 1);
-    memcpy(m, n, sizeof(bool)*N*M);
+
+    // Toggle to alive at random.
+    for (uint32_t i = 0; i < N; i++) {
+        for (uint32_t j = 0; j < M; j++) {
+            n[i][j] = (rand() % 100) < INIT_ALIVE_CHANCE;
+        }
+    }
+
+    memcpy(m, n, sizeof(bool) * N * M);
 }
 
 void colors_init() {
@@ -75,14 +97,17 @@ void curses_init() {
     cbreak();
     noecho();
     curs_set(0);
-    timeout(60);
+    timeout(70);
     colors_init();
     getmaxyx(stdscr, N, M);
+    N *= 2;
 }
 
 int main() {
     srand(time(NULL));
+    setlocale(LC_ALL, "");
     curses_init();
+
     bool n[N][M];
     bool m[N][M];
     init(n, m);
